88ac5d7699780b1bebe2d6552e2d7a1e8c52ef5f
Sohana Khatun <83324509+sohanakhatun@users.noreply.github.com>
2023-12-23 19:48:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -49,8 +49,8 @@ app.post("/create-checkout-session", async (req, res) => {
     payment_method_types: ["card"],
     line_items: lineItems,
     mode: "payment",
-    success_url: `http://localhost:3000/success`,
-    cancel_url: `http://localhost:3000/cancel`,
+    success_url: `https://mobile-kart-frontend.vercel.app/success`,
+    cancel_url: `https://mobile-kart-frontend.vercel.app/cancel`,
   });
  
   res.json({id:session.id})
